54184ecbebcd037e88384facb5a5afc055a30aeb
Ray Bunnage <rayb@seattlespineinstitute.com>
2013-07-10 15:08:51 -0700

    let client id be read only so new will work

--- app/models/foo.rb
+++ app/models/foo.rb
@@ -1,6 +1,7 @@
 class Foo < ActiveRecord::Base
   #has_many :bars, autosave: true, dependent: :destroy
-  attr_accessible :foo_date, :is_happy, :name, :age, :client_id, :power
+  attr_accessible :foo_date, :is_happy, :name, :age,  :power
 									#:bars,
 									#:bars_attributes
+  attr_accessor :client_id
 end
