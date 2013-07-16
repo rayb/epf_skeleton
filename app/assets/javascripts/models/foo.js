1589df9270f2c85d491cdb02e526ab4d583e4990
Ray Bunnage <rayb@seattlespineinstitute.com>
2013-07-15 17:54:13 -0700

    switch to js file to overcome number/boolean bug, then how to reopen and forward reference a model

---
+++ app/assets/javascripts/models/foo.js
@@ -0,0 +1,14 @@
+App.Bar = Ep.Model.extend();
+
+App.Foo = Ep.Model.extend({
+  name: Ep.attr('string'),
+  power: Ep.attr('string'),
+    age: Ep.attr('number'),
+  is_happy: Ep.attr('boolean'),
+    bars: Ep.hasMany(App.Bar)
+})
+
+App.Bar.reopen ({
+  foo: Ep.belongsTo(App.Foo),
+  name: Ep.attr('string')
+})
