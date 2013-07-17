a68f0180e5e49b26b224c0d0344b2d42da639295
Sophia Anopa <sanopa@gmail.com>
2013-07-17 16:51:24 -0400

    adding more data examples

--- db/seeds.rb
+++ db/seeds.rb
@@ -7,6 +7,7 @@
 #   Mayor.create(name: 'Emanuel', city: cities.first)
 dan = Foo.find_or_create_by_name_and_age_and_power("dan", 42, 'thunder')
 ray = Foo.find_or_create_by_name_and_age_and_power("ray", 13, 'lightning')
+sonya = Foo.find_or_create_by_name_and_age_and_power_and_is_happy("sonya", 18, 'eating', true)
 
 dan.update_attributes( is_happy: true)
 ray.update_attributes( is_happy: false)
@@ -16,3 +17,6 @@ bar2 = Bar.find_or_create_by_foo_id_and_name_and_bar_date(dan.id, "dan_bar_2", "
 bar3 = Bar.find_or_create_by_foo_id_and_name_and_bar_date(dan.id, "dan_bar_3", "2011-05-25")
 bar4 = Bar.find_or_create_by_foo_id_and_name_and_bar_date(ray.id, "ray_bar_1", "2046-11-23")
 bar5 = Bar.find_or_create_by_foo_id_and_name_and_bar_date(ray.id, "ray_bar_2", "4362-12-24")
+bar6 = Bar.find_or_create_by_foo_id_and_name_and_bar_date(sonya.id, "end of the world bar", "2012-12-21")
+bar7 = Bar.find_or_create_by_foo_id_and_name_and_bar_date(sonya.id, "bar on fifth avenue", "1969-08-09")
+bar8 = Bar.find_or_create_by_foo_id_and_name_and_bar_date(sonya.id, "bar on broadway", "6666-03-22")
