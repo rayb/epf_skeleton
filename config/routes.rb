037f8e413afce9fe34586f15e2bf176310243009
Ray Bunnage <rayb@seattlespineinstitute.com>
2013-07-17 15:45:30 -0700

    cleaned out unused code

--- config/routes.rb
+++ config/routes.rb
@@ -1,21 +1,7 @@
 EpfSkeleton::Application.routes.draw do
 
-  class FormatTest
-    attr_accessor :mime_type
-
-    def initialize(format)
-      @mime_type = Mime::Type.lookup_by_extension(format)
-    end
-
-    def matches?(request)
-      request.format == mime_type
-    end
-  end
-
   root :to => 'application#index'
   resources :foos
   resources :bars
-
-  #get '*asd', :to => 'application#index'
 
 end
