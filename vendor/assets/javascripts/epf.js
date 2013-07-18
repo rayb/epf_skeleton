2a2bb650c44b837d618dff36f82d7c62d4b1f122
Sophia Anopa <sanopa@gmail.com>
2013-07-18 15:38:29 -0400

    cheating way to get api working

--- vendor/assets/javascripts/epf.js
+++ vendor/assets/javascripts/epf.js
@@ -1922,6 +1922,7 @@
                 this._super.apply(this, arguments);
                 this._embeddedManager = Ep.EmbeddedManager.create({ adapter: this });
                 this._pendingOps = {};
+                this.namespace = "api";
             },
             load: function (type, id) {
                 var root = this.rootForType(type), adapter = this;
